5d2d3266c777015843e93eace4823e59be382f44
sihanwang <ocpwj@hotmail.com>
2020-04-14 10:30:02 +0800

    init

---
+++ src/main/java/com/sihan/study/spring/SpringScanningWiring/SpringScanningWiringApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.spring.SpringScanningWiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScanningWiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScanningWiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/spring/SpringScanningWiring/SpringScanningWiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.spring.SpringScanningWiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScanningWiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
